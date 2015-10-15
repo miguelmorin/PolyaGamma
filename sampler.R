3c8fc95c17ea3606bdd7f47a24b6249bdbce4565
Sherman Ip <sip@tinamou.stats.ox.ac.uk>
2015-10-15 12:46:52 +0100

    Added comments

--- sampler.R
+++ sampler.R
@@ -6,4 +6,4 @@ single_sample_naive_polyagamma = function(z, max_k = 100){
   g = rexp(max_k, 1)
   out = 1 / (2*pi**2) * sum(g / (0.25*(1:max_k - 1)**2 + z**2 / (4*pi**2)))
   return(out)
-}
+}#end function
