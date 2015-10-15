7fe0370b6eec8c626bd9cb01ef13f7b47a56f36a
kasparmartens <kaspar.martens@gmail.com>
2015-10-15 15:55:08 +0100

    Bug fix

--- sampler.R
+++ sampler.R
@@ -2,8 +2,8 @@
 
 # Naive sampler for PG(1, z)
 # (based on the finite approximation of infinite sum)
-single_sample_naive_polyagamma = function(z, max_k = 100){
+rpolyagamma_naive = function(z, max_k = 100){
   g = rexp(max_k, 1)
-  out = 1 / (2*pi**2) * sum(g / (0.25*(1:max_k - 1)**2 + z**2 / (4*pi**2)))
+  out = 1 / (2*pi**2) * sum(g / ((1:max_k - 1/2)**2 + z**2 / (4*pi**2)))
   return(out)
 }#end function
