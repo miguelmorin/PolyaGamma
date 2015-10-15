b7403624fda547cbfdb9ec6e10bb06fc68c4b4ae
kasparmartens <kaspar.martens@gmail.com>
2015-10-15 12:35:33 +0100

    Added a naive sampler for PG(1,z)

--- sampler.R
+++ sampler.R
@@ -1,1 +1,9 @@
 # Here will be some functions for Polya-Gamma Gibbs sampling
+
+# Naive sampler for PG(1, z)
+# (based on the finite approximation of infinite sum)
+single_sample_naive_polyagamma = function(z, max_k = 100){
+  g = rexp(max_k, 1)
+  out = 1 / (2*pi**2) * sum(g / (0.25*(1:max_k - 1)**2 + z**2 / (4*pi**2)))
+  return(out)
+}
