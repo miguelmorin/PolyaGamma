f49e1c8a58a0140406a70d381422a5b1673c0724
shermanip <sherman.ip@spc.ox.ac.uk>
2015-10-18 13:10:20 +0100

    classifier experiment now predicts the response vector using the sample mean logistic regression

--- classifierExperiment.R
+++ classifierExperiment.R
@@ -2,6 +2,9 @@
 #DATE: 16/10/15
 
 classifierExperiment = function(){
+  #set random seed
+  set.seed(92486);
+  
   #read data
   data = read.table("SAheart.data",sep=",",head=T,row.names=1);
   #get the response vector
@@ -16,7 +19,7 @@ classifierExperiment = function(){
   p = ncol(X)+1;
   
   #get size of training and testing set
-  n_train = round(0.75*n_total);
+  n_train = round(0.5*n_total);
   n_test = n_total - n_train;
   
   #assign data to training/testing set
@@ -39,65 +42,74 @@ classifierExperiment = function(){
   X_test = cbind(1,X_test);
   
   #get error of logistic regression
-  logistic_train_error = getTestError(X_train,y_train,beta_logistic);
-  logistic_test_error = getTestError(X_test,y_test,beta_logistic);
+  logistic_train_error = getTestError(y_train,as.vector(logisticRegression(X_train,beta_logistic)));
+  logistic_test_error = getTestError(y_test,as.vector(logisticRegression(X_test,beta_logistic)));
   
   #get range of lambda to investigate
-  lambda_exp_vector = seq(1,5);
+  lambda_exp_vector = seq(3,10);
   
-  n_error = 20; #number of chains to run
-  n_samples = 50; #number of betas to sample from the chain
-  n_chain = 100; #the length of the chain
+  n_error = 20; #number of times to repeat the experiment
+  n_samples = 100; #number of betas to sample from the chain
+  n_chain = 150; #the length of the chain
   
   #create matrix to stroe training and testing error
-  test_error = matrix(0,nrow=length(lambda_exp_vector),ncol=n_error);
+  test_error = matrix(0,ncol=length(lambda_exp_vector),nrow=n_error);
   train_error = test_error;
   
   #for every lambda
   for (i in 1:length(lambda_exp_vector)){
     #get lambda
     lambda = lambda_exp_vector[i];
-    
-    #for n_error times
+    #repeat n_error times
     for (j in 1:n_error){
       #get a chain
       chain = gibbs_sampler(y_train, X_train, b = replicate(p,0), B = diag(replicate(p,lambda)),n_chain)$beta;
-      #take the last part of the chain and take the sample mean
-      beta_posterior = colMeans(chain[(n_chain-n_samples):n_chain,]);
-      #get the traing and testing error
-      train_error[i,j] = getTestError(X_train,y_train,beta_posterior);
-      test_error[i,j] = getTestError(X_test,y_test,beta_posterior);
+      #take the last part of the chain
+      beta_posterior = chain[(n_chain-n_samples+1):n_chain,];
+      #average the logistic regression, round it and use it for prediction
+      train_error[j,i] = getTestError(y_train,(colMeans(logisticRegression(X_train,beta_posterior))));
+      test_error[j,i] = getTestError(y_test,(colMeans(logisticRegression(X_test,beta_posterior))));
     }#end for
   }#end for
   
   #plot the training and testing error
   par(mfrow=c(1,2));
-  boxplot(t(train_error),names=paste("10E",sapply(lambda_exp_vector,toString),sep=""),xlab="Prior variance",ylab="Training error");
+  boxplot(train_error,names=paste("10E",sapply(lambda_exp_vector,toString),sep=""),xlab="Prior variance",ylab="Training error");
   abline(logistic_train_error,0,col="red")
-  boxplot(t(test_error),names=paste("10E",sapply(lambda_exp_vector,toString),sep=""),xlab="Prior variance",ylab="Testing error");
+  boxplot(test_error,names=paste("10E",sapply(lambda_exp_vector,toString),sep=""),xlab="Prior variance",ylab="Testing error");
   abline(logistic_test_error,0,col="red");
 }#end classifierExperiment
 
-#FUNCTION: get test error of classifing y with design matrix X using the logistic regression with parameter beta
-getTestError = function(X,y,beta){
+#FUNCTION: get test error of classifing y using prediction yHat
+getTestError = function(y,yHat){
   
-  #check X is a vector, y is a binary vector, beta is a vector
-  if ((!is.matrix(X))|(!is.vector(y))|(!is.vector(beta))|!all((y==0)|(y==1))){
+  #check y is a binary vector, yHat is a vector between 0 and 1
+  if ( (!is.vector(y))|(!is.vector(yHat))| (!all((y==0)|(y==1))) | (!all((yHat>=0)|(yHat<=1))) ){
     stop("Parameters in getTestError are not of the correct type");
   }#end if
   
-  #check X, y and beta have the correct dimenstions
+  #check y and yHat have the correct dimenstions
   n = length(y);
-  p = length(beta);
-  if ( (n!=nrow(X)) | (p!=ncol(X)) ){
+  if ( (n!=length(yHat)) ){
     stop("Dimensions are not of the correct size in getTestError");
   }#end if
   
-  #get vector of symanatic components
-  eta = X %*% beta;
-  #get vector of logistic regression
-  p = 1/(1+exp(-eta));
   #return error
-  return(sum(y!=round(p))/n);
+  return(sum(y!=round(yHat))/n);
   
 }#end getTestError
+
+
+#FUNCTION: return the logistic regression for multiple betas
+#PARAMETERS: X and beta_design are design matrices
+logisticRegression = function(X,beta_design){
+  #check if X and beta_design are matrices
+  if ( (!(is.matrix(X)|is.vector(X))) | (!(is.matrix(beta_design)|is.vector(beta_design))) ){
+    stop("Parameters in logisticRegression are not of the correct type");
+  }#end if
+  #get a matrix of etas
+  eta = beta_design %*% t(X);
+  #return logistic regression
+  p = 1/(1+exp(-eta));
+  return(p);
+}#end logisticRegression
