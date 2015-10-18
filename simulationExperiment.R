97e56ce1637d1e6e959a5457776661fc4caf7794
kasparmartens <kaspar.martens@gmail.com>
2015-10-18 20:02:25 +0100

    Generate data from a simple model

---
+++ simulationExperiment.R
@@ -0,0 +1,13 @@
+logistic = function(x)1/(1+exp(-x))
+
+# Simulate data from a simple model
+generate_from_simple_logistic_model = function(n = 1000){
+  x = rnorm(n)
+  X = cbind(1, x)
+  y = rbinom(n, 1, logistic(1 + 1*x))
+  return(list(X=X, y=y))
+}
+
+data = generate_from_simple_logistic_model(1000)
+obj = gibbs_sampler(data$y, data$X, b=rep(0, 2), B=10*diag(2), 500)
+plot(obj)
