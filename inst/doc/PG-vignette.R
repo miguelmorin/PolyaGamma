d6a9ea103ac1609d05330aa88429fb047639cc13
kasparmartens <kaspar.martens@gmail.com>
2015-10-21 17:38:51 +0100

    build vignette

--- inst/doc/PG-vignette.R
+++ inst/doc/PG-vignette.R
@@ -1,1 +1,6 @@
+## ----fig.height=2--------------------------------------------------------
+library(PolyaGamma)
+data = generate_from_simple_logistic_model(n=100)
+obj = gibbs_sampler(data$y, data$X, lambda=0.001, n_iter_total=150, burn_in=50)
+obj
 
